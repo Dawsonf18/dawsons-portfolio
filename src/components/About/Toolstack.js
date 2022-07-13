f4a3a27a9bd696c7da532161afa007dcab2f27f6
Dawson Filion <Dawsonfilion21@gmail.com>
2022-07-13 18:54:15 -0400

    fixing resume feature

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -10,12 +10,15 @@ function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+      <h2>VS Code</h2>
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <h2>Postman</h2>
         <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <h2>Heroku</h2>
         <SiHeroku />
       </Col>
     </Row>
