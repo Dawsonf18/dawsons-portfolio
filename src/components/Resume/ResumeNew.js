cd1d7cb60a8514d01ac6b3c0023ed617499da939
Dawson Filion <Dawsonfilion21@gmail.com>
2022-07-25 13:56:56 -0400

    resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/SoftwareResume(2).pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { pdfjs } from "react-pdf";
+import { pdfjs } from "react-pdf"; 
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -36,7 +36,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
